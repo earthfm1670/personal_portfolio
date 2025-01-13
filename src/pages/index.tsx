a3613a90a231d764e329b98a67807d3ad4bb3b93
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2025-01-13 14:17:57 +0700

    add: Overpass font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { Navbar } from "@/components/Navbar";
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <div>div 1</div>
       <div>div 2</div>
     </>
