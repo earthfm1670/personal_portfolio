f60ddc07928ffa08fb012993a72e62a6da145434
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2025-01-23 00:48:51 +0700

    fix: build error from quotation

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -13,8 +13,8 @@ export function Introduction() {
         </h1>
         <h2 className="text-gray-300 lg:text-3xl mb-2">Full Stack Developer</h2>
         <p className="text-blue-400 lg:text-lg mb-2">
-          "Driven aspiring fullstack developer ready to turn ideas into
-          impactful digital solutions and kickstart a meaningful career."
+          &quot;Driven aspiring fullstack developer ready to turn ideas into
+          impactful digital solutions and kickstart a meaningful career.&quot;
         </p>
         <Link to="projects" smooth={true} duration={500}>
           <div className="my-2 px-6 py-3 border-2 border-blue-400 text-blue-400 font-semibold rounded-full hover:bg-blue-400 hover:text-gray-900 transition-colors duration-300 cursor-pointer">
